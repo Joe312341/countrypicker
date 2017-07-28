950bfcc8be7b1d5680f4870ba1eaf12d97b3c85b
Joe312341 <joeger518@yahoo.de>
2017-07-27 18:12:47 -0700

    added a loadimage component to avoid flag images from previous countries to be visible until the new flag finished loading

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { currencyPropType, languagePropType } from '../utils/customPropTypes';
+import LoadImage from './LoadImageComponent';
 
-const CountryCard = ({ countryName, regionName, image, languages, capitalCity, currencies, population }) => (
-  <div className="thumbnail row card-background">
-    <h2 className="text-center">{countryName}</h2>
-    <img src={image} alt="Country's Flag" className="responsive custom-image-size" />
-    <div className="caption">
-      <p className="text-center">
-        Region: {regionName} -- Population: {population} -- Capital: {capitalCity}
-      </p>
-      <hr />
-      <div className="col-xs-6">
-        <p className="text-center">Languages</p>
-        {languages.map(language => (
-          <div key={language.name}>
-            {language.name}
+class CountryCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+  }
+  render() {
+    const { countryName, regionName, image,
+      languages, capitalCity, currencies, population } = this.props;
+    return (
+      <div className="thumbnail row card-background">
+        <h2 className="text-center">{countryName}</h2>
+        <LoadImage imageLink={image} />
+        <div className="caption">
+          <p className="text-center">
+            Region: {regionName} -- Population: {population} -- Capital: {capitalCity}
+          </p>
+          <hr />
+          <div className="col-xs-6">
+            <p className="text-center">Languages</p>
+            {languages.map(language => (
+              <div key={language.name}>
+                {language.name}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="col-xs-6 left-border">
-        <p className="text-center">Currencies</p>
-        {currencies.map((currency, i) => (
-          <div key={currency.name}>
-            code: {currency.code} <br />
-            name: {currency.name} <br />
-            symbol: {currency.symbol}
-            {i !== currencies.length - 1 && <hr />}
+          <div className="col-xs-6 left-border">
+            <p className="text-center">Currencies</p>
+            {currencies.map((currency, i) => (
+              <div key={currency.name}>
+                code: {currency.code} <br />
+                name: {currency.name} <br />
+                symbol: {currency.symbol}
+                {i !== currencies.length - 1 && <hr />}
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 CountryCard.propTypes = {
   countryName: PropTypes.string.isRequired,
